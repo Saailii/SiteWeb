3bd2aee2fdd09c05a3be961f56315856091f2dd2
Saailii <ilias.taboada@gmail.com>
2024-03-14 22:16:44 +0100

    CheeseBurger

---
+++ my-app/src/datas/imgCaroussel.js
@@ -0,0 +1,17 @@
+const infoImage = [ {
+    id: 1,
+    title: "Premier titre",
+    image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2023/4/17/FN_202302_0780-999_TripleCheeseburger_Alt_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1681757742916.jpeg"
+
+
+},
+
+{
+    id: 2,
+    title: "Deuxième Titre",
+    image: "https://ewscripps.brightspotcdn.com/dims4/default/d490bb9/2147483647/strip/true/crop/1600x900+0+0/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2F68%2Fd2%2F03a972ea4d778abd3e78f7701bb1%2Fcnn-l19jb21wb25lbnrzl2ltywdll2luc3rhbmnlcy9szwrlltfkm2y2nmm0nthlywixntawntizmjg3owm0y2yxyzu5-l19wywdlcy9oxzy3zjlizwexnjbindu3mty2nwjmywjlndlly2iynjc5.JPG"
+}
+
+]
+
+export default infoImage
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,19 +2,29 @@ import Nav from "../components/Nav";
 import "../styles/nav.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import "../styles/home.css"
-
+import { Carousel } from "react-responsive-carousel"
+import infoImage from "../datas/imgCaroussel";
 
 
 
 
 export default function Home() {
  
-
-
     return (
         <div>
         <Nav/>
       
+        <Carousel infiniteLoop={true}>
+        {infoImage.map(slide => {
+            return (
+            <div key={slide.id}>
+                <img src={slide.image} alt=""></img>
+                <h1>{slide.title}</h1>
+            </div>
+            )
+        })}
+
+        </Carousel>
         
         </div>
     )
